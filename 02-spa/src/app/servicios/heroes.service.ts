07c401655674314c1711da3625d73c80009c0bb7
Practicas <gasparzurea1604@gmail.com>
2020-10-07 14:27:48 +0200

    Terminado el buscador de heroes

--- 02-spa/src/app/servicios/heroes.service.ts
+++ 02-spa/src/app/servicios/heroes.service.ts
@@ -63,16 +63,18 @@ export class HeroesService {
     return this.heroes;
   }
 
-  buscarHeroes(termino: string) {
+  buscarHeroes(termino: string): Heroe[] {
     let heroesArr: Heroe[] = [];
     termino = termino.toLowerCase();
 
-    for (let heroe of this.heroe) {
+    for (let heroe of this.heroes) {
       let nombre = heroe.nombre.toLowerCase();
       if (nombre.indexOf(termino) >= 0) {
         heroesArr.push(heroe);
       }
     }
+
+    return heroesArr;
   }
 
   getHeroe(idx: string) {
