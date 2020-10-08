bb089336eabc0efa9c4e5d408bc07c392b212168
Practicas <gasparzurea1604@gmail.com>
2020-10-08 12:56:43 +0200

    Pipes nativos y personalizados

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre: string = 'Capitán América'
+  nombre2: string = 'capItáN amErICA'
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha: Date = new Date();
+  idioma: string = 'es';
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llegaron los datos');
+    }, 4500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+}
---
+++ 03-pipes/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+
+registerLocaleData(localEs);
+registerLocaleData(localFr);
+
+import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CapitalizadoPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
