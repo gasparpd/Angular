acb7a9c89300a9d3b76a08b34092cab1c582d784
Practicas <gasparzurea1604@gmail.com>
2020-10-13 10:50:47 +0200

    Obtener artista de Spotify

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -16,7 +16,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBkXXFspVr4TZb666_0rqpPFLxVbtNOcWOKQmuM-FMCd_uf62k9VgXAgF93_dT4WtcnMT4HJgMzdUtcxUA'
+      'Authorization': 'Bearer BQDGdaY5v6uja83BZpc4IpNr2iqZnFO1W-_f2vTirdO2manwIsQGv3i7X2d5sS2_zkfG-I4mCX83j2AX3X0'
     });
 
     return this.http.get(url, { headers });
@@ -27,8 +27,12 @@ export class SpotifyService {
       .pipe(map(data => data['albums'].items));
   }
 
-  getArtista(termino: string) {
+  getArtistas(termino: string) {
     return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
       .pipe(map(data => data['artists'].items));
+  }
+
+  getArtista(id: string) {
+    return this.getQuery(`artists/${id}`);
   }
 }
