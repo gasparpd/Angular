e197752fc408375c4084ebb1a8075df2cece7f0c
Practicas <gasparzurea1604@gmail.com>
2020-10-14 13:18:23 +0200

    Creación de componentes. Formularios y rutas

--- 08-formularios/src/app/app-routing.module.ts
+++ 08-formularios/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+import { TemplateComponent } from './pages/template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'template', component: TemplateComponent},
+  {path: 'reactivo', component: ReactiveComponent},
+  {path: '**', pathMatch:'full', redirectTo: 'template'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
