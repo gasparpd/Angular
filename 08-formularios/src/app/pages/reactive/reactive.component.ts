8bd3a38dd4c6f41bbb92d6c7d5ede9a3d84d3e92
Practicas <gasparzurea1604@gmail.com>
2020-10-15 13:09:08 +0200

    Reactivo: Validaciones y HTML

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -17,6 +17,18 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido(){
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+
+  get apellidoNoValido(){
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+
+  get correoNoValido(){
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
@@ -27,5 +39,11 @@ export class ReactiveComponent implements OnInit {
 
   guardar(){
     console.log(this.forma);
+
+    if (this.forma.invalid) {
+      Object.values(this.forma.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
   }
 }
